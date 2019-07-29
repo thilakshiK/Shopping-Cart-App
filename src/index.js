fc0164c189e51bcec5ddd3f18c2e87aa2280ee38
Thilakshi Fonseka <2014com2211@gmail.com>
2019-07-29 19:47:09 +0530

    test change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const element = <h1> Hello World </h1>;
-
-console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
+//importing modules
+const x = number => number;
+console.log(x(5));
