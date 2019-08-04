f6e71fc6ee20bddb07e6560e12f40dcbf118cd0a
Thilakshi Fonseka <2014com2211@gmail.com>
2019-08-04 13:16:56 +0530

    created counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  //counter component
+
+  //state = {  }
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Counter;
