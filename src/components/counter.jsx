bbe639221358d490ef1e7dc670df6b2714c60dfb
Thilakshi Fonseka <2014com2211@gmail.com>
2019-08-10 15:38:59 +0530

    implement bootstrap classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   //counter  component
 
-  //state = {  }
+  state = {
+    count: 0
+  }; //includes data needed by Counter component
+
+  styles = {
+    fontSize: 30,
+    fontWeight: "bald"
+  };
+
   render() {
     return (
       //used instead of <div></div>
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+
+        <button className="btn btn-dark btn-sm m-2">Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state; //object destructuring
+    return count == 0 ? "zero" : count;
   }
 }
 
